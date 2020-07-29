10ea5e80889e2a8986392bdd5512371e7102a575
Hugo_Ribeiro <53909859+Lokiatos@users.noreply.github.com>
2020-07-29 19:00:59 -0300

    picking numbers - better code

--- problem_solving/algorithms/picking_numbers/main.py
+++ problem_solving/algorithms/picking_numbers/main.py
@@ -2,21 +2,16 @@
 # Date: 29/07/2020
 
 def pickingNumbers(a):
-    numbers_tested = []
     amount_numbers = 1
     total = 0
-    for index in range(0, len(a)-1):
-        if a[index] not in numbers_tested:
-            numbers_tested.append(a[index])
-            amount_this_number = a.count(a[index])
-            amountplus1 = a.count(a[index] + 1) + amount_this_number
-            amountminus1 = a.count(a[index] - 1) + amount_this_number
-            if amountplus1 >= total:
-                total = amountplus1
-            if amountminus1 >= total:
-                total = amountminus1
-            if total > amount_numbers:
-                amount_numbers = total
-        else:
-            continue
-    return(amount_numbers)
+    unique_numbers = set(a)
+    for number in unique_numbers:
+        plus1 = a.count(number + 1) + a.count(number)
+        minus1 = a.count(number - 1) + a.count(number)
+        if plus1 >= total:
+            total = plus1
+        if minus1 >= total:
+            total = minus1
+        if total > amount_numbers:
+            amount_numbers = total
+    return amount_numbers
