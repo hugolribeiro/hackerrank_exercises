f4ef9a2683763be776d9a7836acb2f3f5fc3e88c
Hugo Ribeiro <53909859+hugolribeiro@users.noreply.github.com>
2021-01-21 09:06:29 -0300

    Create main.py

---
+++ Python/String_Split_and_Join/main.py
@@ -0,0 +1,7 @@
+def split_and_join(line):
+    # write your code here
+    line_splitted = line.split(' ')
+    return ('-').join(line_splitted)
+    
+# Or in one line:
+# return ('-').join(line.split(' ')
