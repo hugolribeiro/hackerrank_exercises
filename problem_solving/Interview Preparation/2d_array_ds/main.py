032523733edfde22be1038f19425c51d296bfa42
Hugo Ribeiro <hugolerib@gmail.com>
2021-04-26 22:16:50 -0300

    2d_array

--- problem_solving/Interview Preparation/2d_array_ds/main.py
+++ problem_solving/Interview Preparation/2d_array_ds/main.py
@@ -1,8 +1,10 @@
 # Complete the hourglassSum function below.
+# Programmer: Hugo Leça Ribeiro
 def hourglassSum(arr):
     hourglasses_sum = []
     for line in range(1, 5):
         for column in range(1, 5):
-            hourglass_sum = arr[line-1][column-1] + arr[line-1][column] + arr[line-1][column+1] + arr[line][column] + arr[line+1][column-1] + arr[line+1][column] + arr[line+1][column+1]
+            hourglass_sum = arr[line-1][column-1] + arr[line-1][column] + arr[line-1][column+1] + arr[line][column] + \
+                            arr[line+1][column-1] + arr[line+1][column] + arr[line+1][column+1]
             hourglasses_sum.append(hourglass_sum)
     return max(hourglasses_sum)
